46e668bc7f5d91e7af3e88f3fa54b78cfbf5a934
user uaers <thatkrsn21@gmail.com>
2023-09-20 15:59:17 +0300

    Добавили задание

---
+++ task1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] randomArray = new int[a];
+
+void mas(int a)
+{
+for (int i = 0; i < a; i++)
+{
+randomArray[i] = new Random().Next(99,999);
+Console.Write(randomArray[i] + " ");
+}
+
+}
+
+int count (int[] randomArray)
+{
+int count = 0;
+for (int i = 0; i < randomArray.Length; i++)
+{
+if (randomArray[i] % 2 == 0)
+count = count + 1;
+}
+return count;
+}
+
+mas(a);
+Console.Write($"\nКоличество чётных чисел в массиве: {count(randomArray)}");
+
